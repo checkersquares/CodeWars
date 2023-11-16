5b905ccb6efaedf0a02d3d06ba507fe8288591bb
Elias Styner <elias.styner@auticon.ch>
2023-11-16 14:39:34 +0100

    another kata

---
+++ Multiples/Multiples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples
+{
+    internal static class Multiples
+    {
+        public static int GetSumOfMultiplesOfThreeAndFive(int value)
+        {
+            if (value < 1) return 0;
+            var res = 0;
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    res += i;
+            }
+            return res;
+        }
+    }
+}
