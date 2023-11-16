44a50c780a788e97ec2bc12ec6872b01c7b85cc5
Elias Styner <elias.styner@auticon.ch>
2023-11-16 13:43:21 +0100

    finished 2 more katas

---
+++ Deadfish/DeadFish.cs
@@ -0,0 +1,34 @@
+﻿namespace Deadfish
+{
+    public static class DeadFish
+    {
+        public static int[] Play(string data)
+        {
+            var res = new int[data.Length];
+            var i = 0;
+            var current = 0;
+            foreach (var c in data.ToCharArray())
+            {
+                switch (c)
+                {
+                    case 'i':
+                        current++;
+                        break;
+                    case 'd':
+                        current--;
+                        break;
+                    case 's':
+                        current *= current;
+                        break;
+                    case 'o':
+                        res[i] = current;
+                        i++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return res.Take(i).ToArray();
+        }
+    }
+}
---
+++ TitleCase/TitleCase.cs
@@ -0,0 +1,23 @@
+﻿namespace TitleCase
+{
+    internal static class TitleCase
+    {
+        public static string ToTitleCase(string title, string minorWords = "")
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                return title;
+            var words = title.Split(' ').Select(x => x.ToLower()).ToArray();
+            var ex = !string.IsNullOrWhiteSpace(minorWords) ? minorWords.Split(' ').Select(x => x.ToLower()) : new List<string>();
+            for (int i = 0; i < words.Count(); i++)
+            {
+                if (i == 0 || !ex.Contains(words[i]))
+                {
+                    var letters = words[i].ToCharArray();
+                    letters[0] = char.ToUpper(letters[0]);
+                    words[i] = new string(letters);
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
